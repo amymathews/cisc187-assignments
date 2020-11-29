99ee9942694db480e1821f552a7187be17ff7f4c
DaveParillo <DaveParillo@noreply.github.com>
2020-11-29 15:41:16 -0800

    fix handling of STL debug symbols

--- cmake/default_settings.cmake
+++ cmake/default_settings.cmake
@@ -1,8 +1,8 @@
 # Set a default build type if none was specified
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-  message(STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
+  message(STATUS "Setting build type to 'Debug' as none was specified.")
   set(CMAKE_BUILD_TYPE
-    RelWithDebInfo
+    Debug
     CACHE STRING "Choose the type of build." FORCE)
   # Set the possible values of build type for cmake-gui, ccmake
   set_property(
@@ -18,4 +18,9 @@ set (CMAKE_CXX_STANDARD 14)
 set (CMAKE_CXX_STANDARD_REQUIRED YES)
 set (CMAKE_CXX_EXTENSIONS OFF)
 set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang" AND
+   CMAKE_BUILD_TYPE STREQUAL "Debug" )
+  set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -D_GLIBCXX_DEBUG")
+endif()
 
